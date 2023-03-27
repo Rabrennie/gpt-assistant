217692a948adbf20e1758793f55cf2583647c6d6
rabrennie <rennierab@gmail.com>
2023-03-28 00:18:32 +0100

    Improve styling on create page

--- src/routes/prompts/new/+page.svelte
+++ src/routes/prompts/new/+page.svelte
@@ -1,5 +1,9 @@
-<form method="POST" action="?/create">
-	<input type="text" name="name" id="name" />
-	<input type="text" name="prompt" id="prompt" />
-	<button type="submit">Save</button>
-</form>
+<div class="mx-auto max-w-screen-xl flex flex-col gap-8 px-4 sm:px-6 lg:px-8">
+	<form method="POST" action="?/create" class="flex flex-col">
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" class="p-3 text-sm border rounded" />
+		<label for="name" class="mt-8">Prompt</label>
+		<textarea name="prompt" id="prompt" class="p-3 text-sm border rounded" />
+		<button type="submit" class="mt-8 bg-blue-500 text-white p-3 rounded">Save</button>
+	</form>
+</div>
